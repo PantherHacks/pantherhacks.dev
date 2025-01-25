8f3f6ab244f0b417d99948a34b1cbbcca1a19faf
dtsivkovski <dtsivkovs@gmail.com>
2025-01-24 20:35:41 -0800

    navbar componentization

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,50 +1,39 @@
 import Image from "next/image";
-import Link from "next/link";
+
+import { NavLink } from "@/components/ui/NavLink";
 
 const Navbar = () => {
   return (
     <nav className="m-[20px] w-[calc(100%_-_40px)] rounded-[10px] fixed top-0 left-0 bg-primary z-[1000] flex items-center h-[70px] box-border">
       <div className="flex items-center overflow-hidden">
-        <Link
-          href="/"
-          className=" flex items-center justify-center h-[70px] px-[10px] hover:bg-[#83022b] hover:rounded-l-[10px]"
-        >
+        <NavLink href="/" variant={"icon"} className="h-[70px] rounded-l-[10px]">
           <Image
             src="/logos/pantherhacks/pantherhacks_mascot_light.png"
             alt="Panther Hacks Logo"
             width={50}
             height={50}
           />
-        </Link>
+        </NavLink>
       </div>
       <div className="flex items-center flex-grow font-TangoSans text-white text-lg">
-        <Link href="#home" className="flex items-center justify-center h-[70px] px-[16px] hover:bg-[#83022b]">
-          Home
-        </Link>
-        <Link href="#about" className="flex items-center justify-center h-[70px] px-[16px] hover:bg-[#83022b]">
-          About
-        </Link>
-        <Link href="#tracks" className="flex items-center justify-center h-[70px] px-[16px] hover:bg-[#83022b]">
-          Tracks
-        </Link>
-        <Link href="#faqs" className="flex items-center justify-center h-[70px] px-[16px] hover:bg-[#83022b]">
-          FAQs
-        </Link>
-        <Link href="#contact" className="flex items-center justify-center h-[70px] px-[16px] hover:bg-[#83022b]">
-          Contact
-        </Link>
-        <Link
+        <NavLink href="#home">Home</NavLink>
+        <NavLink href="#about">About</NavLink>
+        <NavLink href="#tracks">Tracks</NavLink>
+        <NavLink href="#faqs">FAQs</NavLink>
+        <NavLink href="#contact">Contact</NavLink>
+        <NavLink
           href="#"
-          className="flex items-center justify-center h-[70px] px-[16px] hover:bg-[#83022b] font-TangoSansBold text-xl bg-[rgb(75,0,0)] cursor-not-allowed" // TODO: Update this when we get applying working
+          variant={"bold"}
+          className="bg-[rgb(75,0,0)] cursor-not-allowed" // TODO: Update this when we get applying working
         >
           APPLY
-        </Link>
+        </NavLink>
       </div>
       <div className="flex items-center justify-center">
-        <Link href="https://github.com/pantherhacks" target="_blank" className="px-[10px] hover:bg-[#83022b]">
+        <NavLink href="https://github.com/pantherhacks" target="_blank" variant={"icon"}>
           <Image src="./icons/github.svg" alt="GitHub Icon" width={24} height={24} className="filter invert h-[70px]" />
-        </Link>
-        <Link href="https://discord.gg/9NSwX5PxqC" target="_blank" className="px-[10px] hover:bg-[#83022b]">
+        </NavLink>
+        <NavLink href="https://discord.gg/9NSwX5PxqC" target="_blank" variant={"icon"}>
           <Image
             src="./icons/discord.svg"
             alt="Discord Icon"
@@ -52,11 +41,12 @@ const Navbar = () => {
             height={24}
             className="filter invert h-[70px]"
           />
-        </Link>
-        <Link
+        </NavLink>
+        <NavLink
           href="https://www.instagram.com/chapmancsclub/"
           target="_blank"
-          className="px-[10px] hover:bg-[#83022b] rounded-r-[10px]"
+          variant="icon"
+          className="rounded-r-[10px]"
         >
           <Image
             src="./icons/instagram.svg"
@@ -65,7 +55,7 @@ const Navbar = () => {
             height={24}
             className="filter invert h-[70px]"
           />
-        </Link>
+        </NavLink>
       </div>
     </nav>
   );
---
+++ components/ui/NavLink.tsx
@@ -0,0 +1,36 @@
+import * as React from "react";
+import Link, { LinkProps } from "next/link";
+import { cva, type VariantProps } from "class-variance-authority";
+
+function cn(...classes: (string | false | undefined | null)[]) {
+  return classes.filter(Boolean).join(" ");
+}
+
+const navLinkVariants = cva("flex items-center justify-center transition-colors duration-200", {
+  variants: {
+    variant: {
+      default: "h-[70px] px-[16px] hover:bg-[#83022b]",
+      bold: "h-[70px] px-[16px] hover:bg-[#83022b] font-TangoSansBold text-xl",
+      icon: "px-[10px] hover:bg-[#83022b]",
+    },
+  },
+  defaultVariants: {
+    variant: "default",
+  },
+});
+
+export interface NavLinkProps
+  extends Omit<React.AnchorHTMLAttributes<HTMLAnchorElement>, "href">,
+    LinkProps,
+    VariantProps<typeof navLinkVariants> {
+  children?: React.ReactNode;
+  className?: string;
+}
+
+export function NavLink({ href, children, variant, className, ...props }: NavLinkProps) {
+  return (
+    <Link href={href} className={cn(navLinkVariants({ variant }), className)} {...props}>
+      {children}
+    </Link>
+  );
+}
