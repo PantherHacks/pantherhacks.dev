555e7dd394eb343111fb675801b7c6b2e1bb4fec
Dylan Ravel <dylanm.ravel@gmail.com>
2025-01-28 18:43:25 -0800

    Temp fix

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
   return (
     <footer className="flex flex-col justify-center items-center w-full space-y-2 p-4 bg-primary">
       <h1 className="font-TangoSansBold text-xl text-center text-white">PantherHacks 2025</h1>
-      <InterestForm />
+      {/* <InterestForm /> */}
       <h1 className="text-white text-center pt-2">Made with ❤️ by the PantherHacks Team</h1>
       <div className="flex flex-row justify-center items-center space-x-1" aria-label="Social Links">
         <FooterSocialLink href="https://github.com/pantherhacks" aria-label="PantherHacks GitHub">
