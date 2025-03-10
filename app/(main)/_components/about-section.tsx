9d9332e21241a6a9f8d652d55b9516782d3d1abc
Dylan Ravel <dylanm.ravel@gmail.com>
2025-03-10 10:24:42 -0700

    Added some basic starts the sections

---
+++ app/(main)/_components/about-section.tsx
@@ -0,0 +1,9 @@
+const AboutSection = () => {
+  return (
+    <div id="about" className="flex flex-col items-center justify-center w-full py-40 bg-blue-400">
+      About Section
+    </div>
+  );
+};
+
+export default AboutSection;
---
+++ app/(main)/_components/faqs-section.tsx
@@ -0,0 +1,9 @@
+const FaqsSection = () => {
+  return (
+    <div id="faqs" className="flex flex-col items-center justify-center w-full py-40 bg-red-400">
+      FAQs Section
+    </div>
+  );
+};
+
+export default FaqsSection;
