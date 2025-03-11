c848cbef07cf86ecb8912d55ec8079578631bd74
Dylan Ravel <dylanm.ravel@gmail.com>
2025-03-11 15:50:19 -0700

    Fixed transision and added mobile dividers

--- app/(main)/_components/tracks-section.tsx
+++ app/(main)/_components/tracks-section.tsx
@@ -13,6 +13,14 @@ const TracksSection = () => {
         className="hidden md:block w-96 h-96 absolute -left-2 top-0 -translate-y-1/2 select-none"
         draggable={false}
       />
+      <Image
+        src="/images/oranges/orange-branch-mobile-divider.svg"
+        alt="An orange branch divider."
+        width={100}
+        height={100}
+        className="block md:hidden w-80 h-80 sm:w-96 sm:h-96 absolute top-0 -translate-y-1/2 select-none"
+        draggable={false}
+      />
       <h2 className="font-TangoSansBold text-6xl text-[#EE5000] pb-10">Tracks</h2>
       <div className="md:grid md:grid-cols-2 space-y-16 md:space-y-0 md:gap-16">
         <Track name="AI" />
@@ -26,6 +34,14 @@ const TracksSection = () => {
         width={100}
         height={100}
         className="hidden md:block w-96 h-96 absolute -right-2 bottom-0 translate-y-1/2 select-none"
+        draggable={false}
+      />
+      <Image
+        src="/images/oranges/orange-branch-mobile-divider.svg"
+        alt="An orange branch divider."
+        width={100}
+        height={100}
+        className="block md:hidden w-80 h-80 sm:w-96 sm:h-96 absolute bottom-0 translate-y-1/2 select-none"
         draggable={false}
       />
     </div>
