6852cc131241a8feaabb2474b9239e49253f7747
Dylan Ravel <dylanm.ravel@gmail.com>
2025-03-10 20:30:27 -0700

    Implemented date info formatting

--- app/(main)/_components/about-section.tsx
+++ app/(main)/_components/about-section.tsx
@@ -1,11 +1,14 @@
+import { hackathonDateInfo } from "@/lib/dates";
+
 const AboutSection = () => {
   return (
     <div id="about" className="flex flex-col items-center justify-center w-full py-40">
       <h2 className="font-TangoSansBold text-6xl text-primary pb-10">About</h2>
       <p className="mx-24 text-center">
-        PantherHacks 2025 is a 48-hour hackathon (team-based coding competition) held from April 25-27, 2025. It will
-        take place in Sandhu Conference Center for the entirety of the event including project work time, workshops,
-        mentorship, and more! Food will be provided. Read more below about our tracks!
+        PantherHacks {hackathonDateInfo.year} is a 48-hour hackathon (team-based coding competition) held from{" "}
+        {hackathonDateInfo.dateString}. It will take place in Sandhu Conference Center for the entirety of the event
+        including project work time, workshops, mentorship, and more! Food will be provided. Read more below about our
+        tracks!
       </p>
     </div>
   );
