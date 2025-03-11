79800541a10bf80491246ca73f4b8dfa36632532
Dylan Ravel <dylanm.ravel@gmail.com>
2025-03-11 09:43:55 -0700

    FAQ formats on mobile

--- app/(main)/_components/faqs-section.tsx
+++ app/(main)/_components/faqs-section.tsx
@@ -4,8 +4,8 @@ const FaqsSection = () => {
   return (
     <div id="faqs" className="flex flex-col items-center justify-center w-full py-40">
       <h2 className="font-TangoSansBold text-6xl text-primary pb-10">FAQs</h2>
-      <p className="m-4">Got questions? We got answers.</p>
-      <div className="w-1/2">
+      <p className="">Got questions? We got answers.</p>
+      <div className="px-10 w-full md:w-1/2">
         <Accordion type="single" collapsible className="w-full">
           <AccordionItem value="item-1">
             <AccordionTrigger>What is your name?</AccordionTrigger>
