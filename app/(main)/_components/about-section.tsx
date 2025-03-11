8b055bc6f09f0f6dab3a46cc5d743e2cd65964fa
Dylan Ravel <dylanm.ravel@gmail.com>
2025-03-10 19:34:19 -0700

    More formatting changes

--- app/(main)/_components/about-section.tsx
+++ app/(main)/_components/about-section.tsx
@@ -1,7 +1,8 @@
 const AboutSection = () => {
   return (
-    <div id="about" className="flex flex-col items-center justify-center w-full py-40 bg-blue-400">
-      About Section
+    <div id="about" className="flex flex-col items-center justify-center w-full py-40">
+      <h2 className="font-TangoSansBold text-6xl text-primary pb-10">About</h2>
+      <p>More information coming soon. Stay tuned!</p>
     </div>
   );
 };
--- app/(main)/_components/faqs-section.tsx
+++ app/(main)/_components/faqs-section.tsx
@@ -1,7 +1,8 @@
 const FaqsSection = () => {
   return (
-    <div id="faqs" className="flex flex-col items-center justify-center w-full py-40 bg-red-400">
-      FAQs Section
+    <div id="faqs" className="flex flex-col items-center justify-center w-full py-40">
+      <h2 className="font-TangoSansBold text-6xl text-primary pb-10">FAQs</h2>
+      <p>Got questions? We got answers.</p>
     </div>
   );
 };
