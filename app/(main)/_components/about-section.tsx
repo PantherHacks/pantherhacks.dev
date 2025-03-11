9af908ca92342d0cdf207c8a94540f4dec168e59
dtsivkovski <dtsivkovs@gmail.com>
2025-03-11 16:32:19 -0700

    Adjust padding in About section for improved responsiveness

--- app/(main)/_components/about-section.tsx
+++ app/(main)/_components/about-section.tsx
@@ -4,7 +4,7 @@ const AboutSection = () => {
   return (
     <div id="about" className="flex flex-col items-center justify-center w-full py-40">
       <h2 className="font-TangoSansBold text-6xl text-primary pb-10">About</h2>
-      <p className="mx-24 text-center">
+      <p className="mx-8 sm:mx-20 md:mx-24 text-center">
         PantherHacks {hackathonDateInfo.year} is a 48-hour hackathon (team-based coding competition) held from{" "}
         {hackathonDateInfo.dateString}. It will take place in Sandhu Conference Center for the entirety of the event
         including project work time, workshops, mentorship, and more! Food will be provided. Read more below about our
