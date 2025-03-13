70fdc07a760acbd3ea77178bc3f571628feed27a
dtsivkovski <dtsivkovs@gmail.com>
2025-03-13 16:58:00 -0700

    text center for prize title

--- app/(main)/_components/_prizes/prize.tsx
+++ app/(main)/_components/_prizes/prize.tsx
@@ -23,9 +23,7 @@ const Prize: FC<PrizeProps> = ({ title, description, imageSrc, imageSide = "left
         className="w-44 max-w-64 prize-image transform-gpu"
         draggable="false"
       />
-      <div
-        className={`flex flex-col items-center justify-center ${imageSide == "left" ? "text-start" : "text-end"} gap-y-2`}
-      >
+      <div className={`flex flex-col items-center justify-center text-center gap-y-2`}>
         <h2 className="text-2xl font-bold font-TangoSansBold text-primary">{title}</h2>
         <p className="">{description}</p>
       </div>
