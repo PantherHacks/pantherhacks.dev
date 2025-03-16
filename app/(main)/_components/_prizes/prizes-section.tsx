0063097e989910265372cb0a20032e20702c6422
Daniel Tsivkovski <89223402+dtsivkovski@users.noreply.github.com>
2025-03-16 14:43:47 -0700

    Update app/(main)/_components/_prizes/prizes-section.tsx
    Co-authored-by: Dylan Ravel <48571264+DylanDevelops@users.noreply.github.com>

--- app/(main)/_components/_prizes/prizes-section.tsx
+++ app/(main)/_components/_prizes/prizes-section.tsx
@@ -12,7 +12,7 @@ const PrizesSection = () => {
     >
       <div className="flex flex-col items-center justify-center text-center gap-y-4">
         <h2 className="font-TangoSansBold text-6xl  text-primary pb-10">Prizes</h2>
-        <p>One per winning team member!</p>
+        <p>Learn more about the prizes we offer.</p>
       </div>
       <Prize
         title="HACKER'S CHOICE AWARD"
