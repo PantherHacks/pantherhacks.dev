9a6a97f22e34656d4816829e1ba6f571f6b93a94
Dylan Ravel <dylanm.ravel@gmail.com>
2025-03-16 17:45:30 -0700

    Fixes prizes and team member photos

--- app/(main)/_components/_prizes/prize.tsx
+++ app/(main)/_components/_prizes/prize.tsx
@@ -23,6 +23,7 @@ const Prize: FC<PrizeProps> = ({ title, description, imageSrc, imageSide = "left
         height={0}
         className={`w-28 sm:w-40 md:w-44 max-w-64 prize-image transform-gpu transition-transform duration-300 hover:scale-105 ${imageSide == "left" ? "hover:-rotate-6" : "hover:rotate-6"}`}
         draggable="false"
+        unoptimized
       />
       <div className="flex flex-col items-center justify-center text-center gap-y-2">
         <h2 className="text-2xl font-bold font-TangoSansBold text-primary">{title}</h2>
