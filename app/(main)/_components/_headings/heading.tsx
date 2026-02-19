60cfd8f140cb5b79b3b984fd8a07e6e8ff48875c
Daniel Tsivkovski <89223402+dtsivkovski@users.noreply.github.com>
2026-02-18 16:11:31 -0800

    Refactor code structure for improved readability and maintainability

--- app/(main)/_components/_headings/heading.tsx
+++ app/(main)/_components/_headings/heading.tsx
@@ -3,18 +3,19 @@
 import Image from "next/image";
 import { ParallaxBanner, ParallaxBannerLayer } from "react-scroll-parallax";
 
+import SpecialButton from "@/components/ui/special-button";
 import { hackathonDateInfo } from "@/lib/dates";
 
 const Heading = () => {
   return (
     <>
       <ParallaxBanner
-        className="h-[60rem] [@media(min-width:1000px)]:h-[70rem] [@media(min-width:1800px)]:h-[80rem] [@media(min-width:3000px)]:h-[90rem] parallax-banner"
+        className="h-[50rem] [@media(min-width:1000px)]:h-[60rem] [@media(min-width:1800px)]:h-[70rem] [@media(min-width:3000px)]:h-[90rem] parallax-banner"
         style={{
-          background: `linear-gradient(#195B7B 0%, #4DC9CC 30%, #BEFCFD 100%)`,
+          background: `linear-gradient(#0c0a24 0%, #0b0b0b 30%, #000000 100%)`,
         }}
       >
-        <ParallaxBannerLayer speed={1}>
+        {/* <ParallaxBannerLayer speed={1}>
           <Image
             className="absolute w-[200%] top-[22rem] z-10"
             src="/images/main-background/mountain-1-cropped.svg"
@@ -23,24 +24,24 @@ const Heading = () => {
             height={0}
             loading="eager"
           />
-        </ParallaxBannerLayer>
+        </ParallaxBannerLayer> */}
 
         <ParallaxBannerLayer speed={10}>
           <Image
-            className="absolute w-[200%] top-[32rem] z-20"
-            src="/images/main-background/mountain-2-cropped.svg"
-            alt="Mountain"
+            className="absolute w-[200%] top-[22rem] z-20 grayscale"
+            src="/images/main-background/building-background.svg"
+            alt="Buildings"
             width={0}
             height={0}
             loading="eager"
           />
         </ParallaxBannerLayer>
 
-        <ParallaxBannerLayer speed={20}>
-          <div className="absolute w-full bg-[#3F1324] top-[54rem] h-[100rem] z-30" />
-        </ParallaxBannerLayer>
+        {/* <ParallaxBannerLayer speed={20}>
+          <div className="absolute w-full bg-[#000000] top-[62rem] h-[100rem] z-30" />
+        </ParallaxBannerLayer> */}
 
-        <ParallaxBannerLayer speed={30}>
+        {/* <ParallaxBannerLayer speed={30}>
           <Image
             className="absolute w-[200%] top-[48rem] z-40"
             src="/images/main-background/keck-cropped-cropped.svg"
@@ -49,19 +50,23 @@ const Heading = () => {
             height={0}
             loading="eager"
           />
-        </ParallaxBannerLayer>
+        </ParallaxBannerLayer> */}
 
         <ParallaxBannerLayer speed={20}>
           <div className="absolute w-full flex justify-center items-center text-center top-[26rem] z-50">
-            <h1 className="font-TangoSansBold text-3xl sm:text-6xl lg:text-7xl xl:text-8xl text-[#befcfd] max-w-[calc(100vw-4rem)] drop-shadow-lg">
+            <h1 className="font-Xirod text-2xl sm:text-5xl lg:text-6xl xl:text-7xl text-[#fe5401] max-w-[calc(100vw-4rem)] drop-shadow-lg">
               PANTHERHACKS {hackathonDateInfo.year}
             </h1>
           </div>
         </ParallaxBannerLayer>
 
+        <div className="absolute w-full flex flex-col justify-center items-center text-center gap-y-8 top-[36rem] z-[70]">
+          <SpecialButton className="font-Xirod text-4xl">APPLY NOW</SpecialButton>
+        </div>
+
         <ParallaxBannerLayer speed={20}>
-          <div className="absolute w-full flex flex-col justify-center items-center text-center gap-y-8 top-[64rem] [@media(min-width:1000px)]:top-[72rem] [@media(min-width:1550px)]:top-[77rem] [@media(min-width:1800px)]:top-[85rem] z-[60]">
-            <h2 className="font-TangoSansBold text-xl sm:text-5xl text-white cursor-default [@media(min-width:1800px)]:bg-[#3F1324] p-4 rounded-lg">
+          <div className="absolute w-full flex flex-col justify-center items-center text-center gap-y-8 top-[34rem] z-[60]">
+            <h2 className="font-UbuntuMonoBold text-xl sm:text-5xl text-white cursor-default [@media(min-width:1800px)]:bg-[#3F1324] p-4 rounded-lg">
               {hackathonDateInfo.dateString}
             </h2>
           </div>
---
+++ components/ui/special-button.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import { useState } from "react";
+
+interface SpecialButtonProps {
+  children?: React.ReactNode;
+  onClick?: () => void;
+  className?: string;
+}
+
+export default function SpecialButton({ children, onClick, className = "" }: SpecialButtonProps) {
+  const [isHovered, setIsHovered] = useState(false);
+  const [isClicked, setIsClicked] = useState(false);
+  const [wasClicked, setWasClicked] = useState(false); // keeps track of if button was clicked while hovering
+
+  // tailwind class strings
+
+  // text
+  const defaultTextColor = "text-teal-300";
+  const hoverTextColor = "text-orange-600";
+  const clickedTextColor = "text-fuchsia-500";
+
+  // border 1
+  const defaultBorderColor1 = "border-teal-300";
+  const hoverBorderColor1 = "border-orange-600";
+  const clickedBorderColor1 = "border-fuchsia-500";
+
+  // border 2
+  const defaultBorderColor2 = "border-teal-300/50";
+  const hoverBorderColor2 = "border-orange-600/50";
+  const clickedBorderColor2 = "border-fuchsia-500/50";
+
+  // border 3
+  const defaultBorderColor3 = "border-teal-300/30";
+  const hoverBorderColor3 = "border-orange-600/30";
+  const clickedBorderColor3 = "border-fuchsia-500/30";
+
+  // border 4
+  const defaultBorderColor4 = "border-teal-300/10";
+  const hoverBorderColor4 = "border-orange-600/10";
+  const clickedBorderColor4 = "border-fuchsia-500/10";
+
+  return (
+    <button
+      onClick={onClick}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => {
+        setIsHovered(false);
+        setWasClicked(false);
+      }}
+      onMouseDown={() => {
+        setIsClicked(true);
+        setWasClicked(true);
+      }}
+      onMouseUp={() => setIsClicked(false)}
+      className={`relative ${className} px-5 py-4 cursor-pointer transition-all ${isClicked ? `${clickedTextColor}` : isHovered ? `${hoverTextColor} ${!wasClicked ? "" : "delay-300"}` : `${defaultTextColor} delay-300`}`}
+    >
+      {children}
+      <div
+        className={`absolute bottom-0 left-0 w-full h-full rounded-none border-2 transition-all ${isClicked ? `${clickedBorderColor1} scale-[153%] duration-700` : isHovered ? `${hoverBorderColor1} scale-[103%] ${!wasClicked ? "" : "delay-300"}` : `${defaultBorderColor1} delay-300`}`}
+      ></div>
+      <div
+        className={`absolute -bottom-1 left-0 w-full h-full rounded-none border-2 transition-all ${isClicked ? `${clickedBorderColor2} delay-100 scale-[150%] duration-700` : isHovered ? `${hoverBorderColor2} scale-[100%] ${!wasClicked ? "delay-100" : "delay-200"}` : `${defaultBorderColor2} scale-[97%] delay-200`}`}
+      ></div>
+      <div
+        className={`absolute -bottom-2 left-0 w-full h-full rounded-none border-2 transition-all ${isClicked ? `${clickedBorderColor3} delay-200 scale-[147%] duration-700` : isHovered ? `${hoverBorderColor3} scale-[97%] ${!wasClicked ? "delay-200" : "delay-100"}` : `${defaultBorderColor3} scale-[94%]  delay-100`}`}
+      ></div>
+      <div
+        className={`absolute -bottom-3 left-0 w-full h-full rounded-none border-2 transition-all ${isClicked ? `${clickedBorderColor4} delay-300 scale-[144%] duration-700` : isHovered ? `${hoverBorderColor4} scale-[94%] ${!wasClicked ? "delay-300" : ""}` : `${defaultBorderColor4} scale-[91%]`}`}
+      ></div>
+    </button>
+  );
+}
