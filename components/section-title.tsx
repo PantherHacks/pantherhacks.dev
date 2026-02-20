ce52464e2ffa7cab8d14cf28d8c8bfea4bbf7d9f
Daniel Tsivkovski <89223402+dtsivkovski@users.noreply.github.com>
2026-02-19 19:44:37 -0800

    Refactor SectionTitle component for improved styling and layout

--- components/section-title.tsx
+++ components/section-title.tsx
@@ -30,20 +30,20 @@ export const SectionTitle = ({
 
   return (
     <div
-      className={`flex flex-col justify-center items-center w-[32rem] mb-14 ${className}`}
+      className={`flex flex-col scale-50 xs:scale-[65%] sm:scale-[85%] md:scale-100 justify-center items-center w-[32rem] mb-14 ${className}`}
       style={{
-        backgroundColor: color2,
+        background: `linear-gradient(180deg, ${color2} 0%, #00000000 50%, ${color2} 100%)`,
       }}
     >
       <div
-        className="relative z-10 pointer-events-none w-full h-3 -top-4 left-0"
+        className="relative z-10 pointer-events-none w-full h-3 -top-2 left-0"
         style={{
           backgroundColor: color3,
           boxShadow: `0px -4px 4px ${color4}, -1px 0px 2px ${color4}, 1px 0px 2px ${color4}, 0px -0.5px 2px ${color4}`,
         }}
       ></div>
       <h2
-        className={`font-Xirod text-6xl -mb-3 select-none`}
+        className={`font-Xirod text-6xl mt-2 mb-0 select-none`}
         style={{
           color: color1,
           textShadow: `0px -2px 2px ${textShadowColor}, -1px 0px 2px ${textShadowColor}, 0px 0px 1px ${textShadowColor}, 0px -0.5px 1px ${textShadowColor}`,
@@ -64,7 +64,7 @@ export const SectionTitle = ({
         })}
       </h2>
       <div
-        className="flex flex-row justify-between px-2 relative z-10 pointer-events-none w-full h-4 -bottom-6 left-0"
+        className="flex flex-row justify-between px-2 relative z-10 pointer-events-none w-full h-4 -bottom-4 left-0"
         style={{
           backgroundColor: color3,
           boxShadow: `0px -4px 4px ${color4}, -1px 0px 2px ${color4}, 1px 0px 2px ${color4}, 0px -0.5px 2px ${color4}`,
