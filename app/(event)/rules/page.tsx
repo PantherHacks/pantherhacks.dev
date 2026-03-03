89ee7aa40c05c15ce4a24560d17413cce6b0f908
Nataniel Farzan <natanielfarzan@gmail.com>
2026-03-02 17:56:36 -0800

    Update rules page

--- app/(event)/rules/page.tsx
+++ app/(event)/rules/page.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 import Footer from "@/components/footer/footer";
 import Navbar from "@/components/navbar/navbar";
 import { formatHourTo12Hour, hackathonDateInfo } from "@/lib/dates";
@@ -27,14 +25,14 @@ const RulesPage = () => {
                   <li>Learn new things.</li>
                   <li>Step outside of your comfort zone.</li>
                   <li>Experiment and make mistakes.</li>
-                  <li>Don’t be afraid to ask for help from fellow hackers, mentors, or organizers.</li>
+                  <li>Don't be afraid to ask for help from fellow hackers, mentors, or organizers.</li>
                 </ul>
               </li>
               <li>
                 <span className="font-bold">Build</span>
                 <ul style={{ listStyleType: "square" }} className="ml-8">
                   <li>Build cool stuff.</li>
-                  <li>If you’re not able to finish your project by the deadline, that’s totally fine.</li>
+                  <li>If you're not able to finish your project by the deadline, that's totally fine.</li>
                 </ul>
               </li>
               <li>
@@ -49,20 +47,18 @@ const RulesPage = () => {
           <div className="flex flex-col flex-1 w-full justify-center items-center mt-10">
             <h3 className="font-Xirod text-2xl pb-5 text-[#EE5000] text-center">Competition Rules</h3>
             <ul style={{ listStyleType: "circle" }} className="list-spacing ml-3">
+              <li>Only current undergraduate students are eligible to participate in the competition as hackers. </li>
+              <li>Teams are limited to a minimum of one person and a maximum of four people.</li>
               <li>
-                Only current students of Chapman University are eligible to participate in the competition as hackers.
-              </li>
-              <li>Team sizes are limited to a minimum of one person and a maximum of four people.</li>
-              <li>
-                Every team is eligible to submit one project to one of the four development tracks (Entertainment,
-                Cybersecurity, Healthcare, Sustainability).
+                Every team is eligible to submit exactly one project to only one of the four development tracks
+                (Cybersecurity, Entertainment, Healthcare, Sustainability).
               </li>
               <li>
                 All team members must be present during event check-in, judging, and the awards ceremony. Leaving the
                 venue during the event to work elsewhere is fine.
               </li>
               <li>
-                Teams are encouraged to receive feedback and support from organizers, volunteers, sponsors, and others.
+                Teams are encouraged to receive feedback and support from fellow hackers, mentors, and organizers.
               </li>
               <li>
                 All project work must be done within the 48-hour hacking period (
@@ -73,9 +69,8 @@ const RulesPage = () => {
                 can use an idea they had before the event.
               </li>
               <li>
-                Teams can work on ideas that have already been done. If somebody wants to work on a common idea, they
-                should be allowed to do so and should be judged on the quality of their hack. These days, it’s hard to
-                find something that’s fully original, and teams might not know that an idea has been done before anyway.
+                Teams can work on ideas that have already been done. If somebody wants to work on a common idea they
+                should be allowed to do so and should be judged on the quality of their hack.
               </li>
               <li>
                 Teams can work on an idea that they have worked on before (as long as they do not reuse any code).
@@ -90,7 +85,7 @@ const RulesPage = () => {
                 introduced or new features added during the hackathon in determining the winners.
               </li>
               <li>
-                Teams can be disqualified from the competition at the organizers’ discretion. Reasons might include but
+                Teams can be disqualified from the competition at the organizers' discretion. Reasons might include but
                 are not limited to violating one or more of the above competition rules.
               </li>
             </ul>
