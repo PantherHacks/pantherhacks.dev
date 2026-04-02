da46518cfdd7a722619d6b94ae8f31f5b011194c
Daniel Tsivkovski <89223402+dtsivkovski@users.noreply.github.com>
2026-04-02 11:14:18 -0700

    adjusted image sizing and countdown alignment on live page

--- app/(event)/live/page.tsx
+++ app/(event)/live/page.tsx
@@ -13,9 +13,9 @@ import ScheduleSection from "./_components/schedule";
 
 const LivePage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center overflow-x-hidden bg-black text-white">
+    <div className="flex min-h-screen flex-col items-center justify-center overflow-x-hidden bg-[#01050B] text-white">
       <Navbar />
-      <main className="flex flex-1 flex-col" id="live-schedule" aria-label="Live Schedule Section">
+      <main className="flex flex-1 flex-col w-full" id="live-schedule" aria-label="Live Schedule Section">
         <ParallaxProvider>
           <LiveHeading />
         </ParallaxProvider>
